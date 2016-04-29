5333dd84c7fc0dd5a49f4b476eede5aaf5888e6d
Théo Mainguet <theo.mainguet@yahoo.fr>
2016-04-29 11:47:23 +0200

    Q4

--- src/Adapters.java
+++ src/Adapters.java
@@ -1,7 +1,7 @@
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 
 import javax.swing.JFrame;
 import javax.swing.JSlider;
@@ -10,16 +10,29 @@ import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
 
-public class Listeners implements MouseListener{
+public class Adapters {
 	
+	private MouseAdapter adapter;
 	private JSlider slider;
 	private Label label;
 
-	public Listeners() {
+	public Adapters() {
 		JFrame fenetre = new JFrame("Listeners");
 		slider = new JSlider(1,100);
 		label = new Label(""+slider.getValue());
 		this.label.getJLabel().setHorizontalAlignment(SwingConstants.CENTER);
+		adapter = new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+				int button = e.getButton();
+				if (button == MouseEvent.BUTTON1 && (e.getX() == label.getJLabel().getX()) && (e.getY() == label.getJLabel().getY())){
+					label.setValeur(1);
+				}else if(button == MouseEvent.BUTTON3 && (e.getX() == label.getJLabel().getX()) && (e.getY() == label.getJLabel().getY())){
+					label.setValeur(-1);
+				}
+				slider.setValue(Integer.parseInt(label.toString()));
+			}
+		};
+		fenetre.addMouseListener(adapter);
 		fenetre.setPreferredSize(new Dimension(200, 80));
 		fenetre.pack();
 		fenetre.setLocationRelativeTo(null);
@@ -28,46 +41,20 @@ public class Listeners implements MouseListener{
 		fenetre.setVisible(true);
 		fenetre.add(label.getJLabel(),BorderLayout.CENTER);
 		fenetre.add(slider,BorderLayout.NORTH);
-		fenetre.addMouseListener(this);
 		slider.addChangeListener(new ChangeListener() {
 			public void stateChanged(ChangeEvent arg0) {
 				label.setLabel(""+slider.getValue());
 			}
 		});
 	}
-
 	
 	public static void main(String[] args) {
 		javax.swing.SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
-				new Listeners();
+				new Adapters();
 			}
 		});
 	}
 
-	public void mouseClicked(MouseEvent e) {
-		int button = e.getButton();
-		if (button == MouseEvent.BUTTON3){
-			label.setValeur(1);
-		}else{
-			label.setValeur(-1);
-		}
-		slider.setValue(Integer.parseInt(label.toString()));
-	}
 
-	public void mouseEntered(MouseEvent e) {
-		
-	}
-
-	public void mouseExited(MouseEvent e) {
-		
-	}
-
-	public void mousePressed(MouseEvent e) {
-		
-	}
-
-	public void mouseReleased(MouseEvent e) {
-		
-	}
 }
--- src/Label.java
+++ src/Label.java
@@ -1,19 +1,33 @@
 import javax.swing.JLabel;
 
-
 public class Label {
 
 	private JLabel label;
-	
+	private int valeur;
+
 	public Label(String valeur) {
 		label = new JLabel(valeur);
 	}
 
-	public JLabel getJLabel(){
+	public JLabel getJLabel() {
 		return this.label;
 	}
-	
-	public void setLabel(String valeur){
+
+	public void setValeur(int i){
+		int a = Integer.parseInt(label.getText()) + i;
+		label.setText(""+a);
+		if(Integer.parseInt(label.getText()) + i < 0){
+			label.setText("" + 0);
+		} else if(Integer.parseInt(label.getText()) + i > 100){
+			label.setText("" + 100);
+		}
+	}
+
+	public void setLabel(String valeur) {
 		label.setText(valeur);
+	}
+
+	public String toString() {
+		return label.getText();
 	}
 }
